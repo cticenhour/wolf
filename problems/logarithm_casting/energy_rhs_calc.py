44736f44eaa6f9fcc38468c509ecc3a102199e86
Casey Icenhour <cticenho@ncsu.edu>
2018-09-19 12:13:47 -0600

    Correct errors in energy MMS python script and input file.

--- problems/logarithm_casting/energy_rhs_calc.py
+++ problems/logarithm_casting/energy_rhs_calc.py
@@ -11,7 +11,7 @@ mui = 1.444e3
 N = 3.22e16
 ki = 6.02909e-17
 kex = 1.35369e-16
-V = 0.2 * (-x*x + 2.54*x)
+V = 0.2 * (-x * x + 2.54 * x)
 ne = 2e7 * (-x * x + 2.54 * x) + 1000
 ni = 2e7 * (-x * x + 2.54 * x) + 1000
 energy = exp(0.865049 * x) + 3
@@ -34,7 +34,7 @@ ni_MMS_rhs = diff(lhs_ni_preDiff, x) - rxn_ni
 
 V_MMS_rhs = -diff2_V - (e / eps) * (ni - ne)
 
-energy_flux_preDiff = -(5.0 / 3.0) * De * ne * diff_energy + (5.0 / 3.0) * lhs_ne_preDiff
+energy_flux_preDiff = -De * ne * diff_energy + (5.0 / 3.0) * energy * lhs_ne_preDiff
 energy_ne_flux = e * lhs_ne_preDiff * diff_V
 energy_rxns = 11.56 * kex * ne + 15.7 * ki * N * ne
 energy_MMS_rhs = diff(energy_flux_preDiff, x) - energy_ne_flux + energy_rxns
